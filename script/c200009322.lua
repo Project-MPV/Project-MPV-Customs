58a343369dbede950473ca36c2b7545b93163d8e
Autumn-Clearwater <dimitriwhisenhunt@gmail.com>
2022-08-30 22:24:09 -0500

    Update c200009322.lua

--- script/c200009322.lua
+++ script/c200009322.lua
@@ -18,8 +18,8 @@ function s.initial_effect(c)
     e2:SetOperation(s.spop)
     c:RegisterEffect(e2)
 end
-s.listed_names={CARD_ASSAULT_MODE,CARD_BAHAMUT}
-s.assault_mode=CARD_BAHAMUT
+s.listed_names={CARD_ASSAULT_MODE,200009310}
+s.assault_mode=200009310
 
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
@@ -35,7 +35,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 
 function s.spfilter(c,e,tp)
-	return c:IsCode(CARD_BAHAMUT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsCode(200009310) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
