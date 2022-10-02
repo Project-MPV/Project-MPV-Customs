38f71a64f61dcddf5cec786ef186df0dea23e5d0
Autumn-Clearwater <dimitriwhisenhunt@gmail.com>
2022-10-01 21:10:59 -0500

    Update c200009318.lua

--- script/c200009318.lua
+++ script/c200009318.lua
@@ -5,17 +5,17 @@ function s.initial_effect(c)
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
     e1:SetRange(LOCATION_REMOVED)
     e1:SetCondition(s.condition)
-    e1:SetCondition(s.target)
+    e1:SetTarget(s.target)
     e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_REMOVE)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
     e2:SetCode(EVENT_PHASE+PHASE_END)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCondition(s.condition)
@@ -27,7 +27,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tp==Duel.GetTurnPlayer()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return end
+	if chk==0 then return  e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
@@ -40,11 +40,11 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,0,0)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,0,0)
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-    if e:GetHandler():IsRelateToEffect(e) then
-        Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
-    end
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+	end
 end
