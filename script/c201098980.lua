c1544ea462a2acd9023b33239a4e1388f2e0b452
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2022-10-13 11:18:56 +0700

    Add files via upload

---
+++ script/c201098980.lua
@@ -0,0 +1,90 @@
+--Imaginary Force - Truthseeker Owl
+local s,id=GetID()
+function s.initial_effect(c)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetLabel(0)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.summon)
+	c:RegisterEffect(e2)
+	--search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TODECK)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCountLimit(1,id+2)
+	e3:SetCode(EVENT_TO_DECK)
+	e3:SetCondition(s.hdcon)
+	e3:SetTarget(s.hdtg)
+	e3:SetOperation(s.hdop)
+	c:RegisterEffect(e3)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.cfilter(c,e,tp,ft)
+	local lv=c:GetOriginalLevel()
+	local rc=c:GetOriginalRace()
+	local att=c:GetOriginalAttribute()
+	local eff4064256={Duel.GetPlayerEffect(tp,4064256)}
+	for _,te in ipairs(eff4064256) do
+		local val=te:GetValue()
+		if val and val(te,c,e,0) then rc=val(te,c,e,1) end
+	end
+	local eff12644061={Duel.GetPlayerEffect(tp,12644061)}
+	for _,te in ipairs(eff12644061) do
+		local val=te:GetValue()
+		if val and val(te,c,e,0) then att=val(te,c,e,1) end
+	end
+	return c:GetOriginalType()&TYPE_MONSTER~=0 and lv>0 and c:IsFaceup() and c:IsAbleToGraveAsCost() and (ft>0 or c:GetSequence()<5)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,lv*2,rc,att,e,tp)
+end
+function s.spfilter(c,lv,rc,att,e,tp)
+	return c:IsSetCard(0x303)and c:IsLevel(lv) and c:IsRace(rc) and c:IsAttribute(att) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return ft>-1 and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp,ft)
+	end
+	e:SetLabel(0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp,ft)
+	local tc=g:GetFirst()
+	Duel.SendtoGrave(tc,REASON_COST)
+	Duel.SetTargetCard(tc)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.summon(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local tc=Duel.GetFirstTarget()
+	if not tc or not tc:IsLocation(LOCATION_GRAVE) or not tc:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetLevel()*2,tc:GetRace(),tc:GetAttribute(),e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.hdcon(e,tp,eg,ep,ev,re,r,rp)
+	if not re then return false end
+	local rc=re:GetHandler()
+	return rc:IsSetCard(0x303)
+end
+function s.hdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,0,1-tp,1)
+end
+function s.hdop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND):RandomSelect(tp,1)
+	Duel.SendtoDeck(g,nil,1,REASON_EFFECT)
+end
