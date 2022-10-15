7911864215967a12c5d8f471710d6d60c5a84f8f
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2022-10-15 07:50:54 +0700

    Updates

--- script/c8999.lua
+++ script/c8999.lua
@@ -3,19 +3,26 @@ function s.initial_effect(c)
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(id,0)
     e1:SetCategory(CATEGORY_DRAW)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_DISCARD or EVENT_TO_DECK)
+    e1:SetCode(EVENT_TO_GRAVE)
+    e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
     e1:SetCondition(s.drawsetup)
+    e1:SetTarget(s.drawtarget)
     e1:SetOperation(s.draw)
+    c:RegisterEffect(e1)
 end
-
-function s.drawsetup(e,tp,eg,ep,ev,re,r,rp,chk)
-    if ckh==0 then return Duel.IsPlayerCanDraw(tp,2) end
+function s.drawsetup(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND) or e:GetHandler():IsPreviousLocation(LOCATION_DECK)
+end
+function s.drawtarget(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
     Duel.SetTargetPlayer(tp)
     Duel.SetTargetParam(2)
     Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
 end
 
 function s.draw(e,tp,eg,ep,ev,re,r,rp)
-    local Duel.Draw(p,d,REASON_EFFECT)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
 end
