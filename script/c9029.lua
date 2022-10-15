3dac6c3ad03c74752613e1452f47736ac15209f7
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2022-10-15 07:44:10 +0700

    Crunk Knight (kind of too much for Rank 3...)

---
+++ script/c9029.lua
@@ -0,0 +1,93 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,3,2)
+	c:EnableReviveLimit()
+	--when Xyz summoned
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_BATTLE_START)
+	e2:SetCondition(s.atkcon)
+	e2:SetOperation(s.tgop)
+	c:RegisterEffect(e2)
+	--chain attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.atcon)
+	e3:SetCost(s.atcost)
+	e3:SetOperation(s.atop)
+	c:RegisterEffect(e3)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(-500)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+			tc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_UPDATE_DEFENSE)
+			tc:RegisterEffect(e2)		
+end
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local hc=Duel.GetAttackTarget()
+	return hc and hc:IsControler(1-tp) and hc:HasLevel()
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+		local bc=e:GetHandler()
+		local hc=Duel.GetAttackTarget()
+		if bc and bc:IsFaceup() and hc:IsRelateToBattle() and bc:IsRelateToBattle() and hc:IsFaceup() then
+			local e1=Effect.CreateEffect(bc)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(600)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+			bc:RegisterEffect(e1)
+end
+end
+function s.atcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return bc:IsLocation(LOCATION_GRAVE) and bc:IsType(TYPE_MONSTER) and c:CanChainAttack() and c:IsStatus(STATUS_OPPO_BATTLE)
+end
+function s.atcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.atop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChainAttack()
+end
