762278c7b22b3fb8e6bbd6196871676f24c776db
Autumn-Clearwater <dimitriwhisenhunt@gmail.com>
2022-10-20 15:24:15 -0500

    Update c303.lua

--- script/c303.lua
+++ script/c303.lua
@@ -3,12 +3,8 @@ function c303.initial_effect(c)
 --Fusion Procedure
 	c:EnableReviveLimit()
 	Fusion.AddProcMixN(c,true,true,200000301,3)
-c:SetUniqueOnField(1,1,aux.FilterBoolFunction(Card.IsCode(),LOCATION_MZONE)
-end
-s.material_setcode=0xdd
-s.listed_name={200000301}
-end
---special summon condition
+	c:SetUniqueOnField(1,1,aux.FilterBoolFunction(Card.IsCode(),LOCATION_MZONE)
+	--special summon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
@@ -23,9 +19,15 @@ end
 	e2:SetValue(s.val)
 	c:RegisterEffect(e2)
 end
+
+s.material_setcode=0xdd
+s.listed_name={200000301}
+
+
 function s.val(e,c)
 	return Duel.GetMatchingGroupCount(s.filter,c:GetControler(),0,LOCATION_GRAVE,nil)*600
 end
+
 function s.filter(c)
 	return c:IsSetCode(0xdd) or c:IsSetCode(0x776e) or c:IsSetCode(0x140) and (c:IsLocation(LOCATION_GRAVE)
 end
