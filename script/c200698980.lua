2aea0c241c0307839a371a3ea9e08086f76389d9
Fawwazzed777 <fawwazzed@gmail.com>
2022-10-20 13:53:42 +0700

    Update c200698980.lua

--- script/c200698980.lua
+++ script/c200698980.lua
@@ -1,10 +1,11 @@
---Imaginary Force
+--Imaginary Force Unify
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x303),matfilter=Card.IsAbleToDeck,extraop=Fusion.ShuffleMaterial})
+	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x303),matfilter=Card.IsAbleToDeck,extrafil=s.extrafil,extraop=Fusion.ShuffleMaterial})
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,aux.FALSE)
 	--draw
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
@@ -12,11 +13,28 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetCode(EVENT_TO_DECK)
-	e2:SetCountLimit(1,id+2)
+	e2:SetCountLimit(1,id)
 	e2:SetCondition(s.drcon)
 	e2:SetTarget(s.drtg)
 	e2:SetOperation(s.drop)
 	c:RegisterEffect(e2)
+end
+s.listed_series={0x303}
+function s.check(g1,g2)
+	return function(tp,sg,fc)
+		local c1=#(sg&g1)
+		local c2=#(sg&g2)
+		return c1<=1 and c2<=1,c1>1 or c2>1
+	end
+end
+function s.extrafil(e,tp,mg)
+	if Duel.GetCustomActivityCount(id,1-e:GetHandlerPlayer(),ACTIVITY_CHAIN)==0 then return nil end
+	local loc=LOCATION_HAND
+	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		loc=loc|LOCATION_ONFIELD
+	end
+	local g=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToDeck),tp,loc,0,mg)
+	return g,s.check(g:Split(Card.IsLocation,nil,LOCATION_ONFIELD))
 end
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
 	if not re then return false end
