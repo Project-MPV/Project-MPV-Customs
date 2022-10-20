8fdbbdc680f5831119367e23ad0c9ffaca7f9f94
Autumn-Clearwater <dimitriwhisenhunt@gmail.com>
2022-10-20 13:59:27 -0500

    Update c200000301.lua

--- script/c200000301.lua
+++ script/c200000301.lua
@@ -1,6 +1,6 @@
 --Blue-Eyes Prophesied Dragon
 local s,id=GetID()
-function c200009347.initial_effect(c)
+function s.initial_effect(c)
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x140),1,1,aux.FilterSummonCode(CARD_BLUEEYES_W_DRAGON),1,1)
 	c:EnableReviveLimit()
 --PendulumSummon
