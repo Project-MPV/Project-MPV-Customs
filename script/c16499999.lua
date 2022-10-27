8803deff717eb22d530905b230071d9b2b7b1a43
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2022-10-27 15:20:10 +0700

    Update c16499999.lua

--- script/c16499999.lua
+++ script/c16499999.lua
@@ -111,7 +111,6 @@ function s.pencon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) end
-	Duel.SetOperationInfo(0,CATEGORY_LEAVE_REMOVED,e:GetHandler(),1,0,0)
 end
 function s.penop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
