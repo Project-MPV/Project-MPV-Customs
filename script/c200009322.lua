64ba0fb5c1a0395ddb8e4e9cc6b4c2bbaaf644f8
Fawwazzed777 <fawwazzed@gmail.com>
2022-10-27 12:22:38 +0700

    Update c200009322.lua
    Completing the unfinished code

--- script/c200009322.lua
+++ script/c200009322.lua
@@ -21,18 +21,22 @@ function s.initial_effect(c)
     e3:SetDescription(aux.Stringid(id,2))
     e3:SetType(EFFECT_TYPE_QUICK_O)
     e3:SetCategory(CATEGORY_ATKCHANGE)
-    e3:SetCode(EVENT_FREE_CHAIN)
+    e3:SetCode(EVENT_ATTACK_ANNOUNCE)
     e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e3:SetHintTiming(TIMING_BATTLE_PHASE)
     e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
     e3:SetCondition(s.atkchngcon)
     e3:SetTarget(s.atkchngtg)
     e3:SetOperation(s.atkchng)
     c:RegisterEffect(e3)
-    local e4=Effect.CreateEffect(c)
-    c:RegisterEffect(e4)
-    local e5=Effect.CreateEffect(c)
-    c:RegisterEffect(e5)
+   	--immune
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EFFECT_IMMUNE_EFFECT)
+	e4:SetValue(s.efilter)
+	c:RegisterEffect(e4)
 end
 s.listed_names={CARD_ASSAULT_MODE,200009310}
 s.assault_mode=200009310
@@ -60,21 +64,32 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-
 function s.atkchngcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsBattlePhase() and not e:GetHandler():IsStatus(STATUS_CHAINING)
+	local at=Duel.GetAttacker()
+	return (at:IsControler(tp) or at:IsControler(1-tp))
 end
 
 function s.atkchngtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return false end
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    if chk==0 then do return bc and bc:IsOnField() and bc:IsCanBeEffectTarget(e) end
-    Duel.SetTargetCard(bc)
-    local g=Group.FromCards(bc)
-    Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,0,0)
+	local tg=Duel.GetAttacker()
+	if chkc then return chkc==tg end
+	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
+	Duel.SetTargetCard(tg)
+	Duel.SetChainLimit(s.chlimit)
 end
-
+function s.chlimit(e,ep,tp)
+	return tp==ep
+end
 function s.atkchng(e,tp,eg,ep,ev,re,r,rp)
-    
+	local tg=Duel.GetAttacker()
+	if tg and tg:IsRelateToBattle() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tg:RegisterEffect(e1)
+end
+end
+function s.efilter(e,re)
+	return e:GetHandlerPlayer()~=re:GetOwnerPlayer()
 end
