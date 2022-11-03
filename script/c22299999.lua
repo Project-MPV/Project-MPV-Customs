097d0c227e0579dadd8ae987461e2fccc375fec7
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2022-11-03 13:25:36 +0700

    Create c22299999.lua

---
+++ script/c22299999.lua
@@ -0,0 +1,94 @@
+--Eternity Ace - Azeya
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetSPSummonOnce(id)
+	--Place "Eternity Tech" Spell/Trap
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.tfcon)
+	e1:SetTarget(s.tftg)
+	e1:SetOperation(s.tfop)
+	c:RegisterEffect(e1)
+	--To pendulum zone, if banished
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCondition(s.pencon)
+	e2:SetTarget(s.pentg)
+	e2:SetOperation(s.penop)
+	c:RegisterEffect(e2)
+	--Attach 1 banished "Eternity Tech" to your Xyz
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_PZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetTarget(s.atcctg)
+	e3:SetOperation(s.atccop)
+	c:RegisterEffect(e3)
+end
+function s.tfcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase()
+end
+function s.tffilter(c,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsType(TYPE_CONTINUOUS) and c:IsSetCard(0x993) and not c:IsForbidden() and c:CheckUniqueOnField(tp)
+end
+function s.tftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tffilter(chkc,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(s.tffilter,tp,LOCATION_REMOVED,0,1,e:GetHandler(),tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectTarget(tp,s.tffilter,tp,LOCATION_REMOVED,0,1,1,e:GetHandler(),tp)
+end
+function s.tfop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFirstTarget()
+	local a,b=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	if a<1 or not g or not g:IsRelateToEffect(e) then return end
+	Duel.MoveToField(g,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+end
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return (r&REASON_EFFECT)~=0
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) end
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.CheckLocation(tp,LOCATION_PZONE,0) and not Duel.CheckLocation(tp,LOCATION_PZONE,1) then return false end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+function s.filter1(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) 
+end
+function s.filter2(c)
+	return c:IsSetCard(0x993)
+end
+function s.atcctg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(s.filter2,tp,LOCATION_REMOVED,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+	local g1=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,4))
+	local g2=Duel.SelectTarget(tp,s.filter2,tp,LOCATION_REMOVED,0,1,1,nil)
+end
+function s.atccop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsImmuneToEffect(e) then return end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,tc,e)
+	if #g>0 then
+		Duel.Overlay(tc,g)
+	end
+end
