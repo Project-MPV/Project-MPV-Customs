eccca0e54b10878bc2f0e00d9c90f6e47c4b9d2b
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2022-12-04 23:12:35 +0700

    added a new card
    Support for Eternity Ace/Tech

---
+++ script/c22499999.lua
@@ -0,0 +1,112 @@
+--Eternity Ace - Full Drive Azeya
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x994),5,4)
+	--Material Check
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_MATERIAL_CHECK)
+	e2:SetValue(s.valcheck)
+	e2:SetLabelObject(e1)
+	c:RegisterEffect(e2)
+	--cannot disable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(s.cnvalue)
+	e3:SetCode(EFFECT_CANNOT_DISEFFECT)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_CANNOT_DISABLE)
+	c:RegisterEffect(e4)
+	--Return and gains ATK
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_TOHAND)
+	e5:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCondition(s.tcon)
+	e5:SetCost(s.tcost)
+	e5:SetTarget(s.ttg)
+	e5:SetOperation(s.top)
+	c:RegisterEffect(e5)
+end
+s.listed_names={22399999}
+function s.cnvalue(e,ct)
+	return Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT):GetHandler()==e:GetHandler()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) and e:GetLabel()==1
+end
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(Card.IsCode,1,nil,22399999,c) then
+		e:GetLabelObject():SetLabel(1)
+	else
+		e:GetLabelObject():SetLabel(0)
+	end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,30459350)
+		and Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD+LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE+LOCATION_EXTRA,0)>0
+		and Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD+LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,LOCATION_ONFIELD+LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE+LOCATION_EXTRA)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_EXTRA)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local sg=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_ONFIELD+LOCATION_GRAVE,LOCATION_ONFIELD+LOCATION_GRAVE,1,1,nil)
+	if #sg>0 then
+		Duel.BreakEffect()
+		local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD+LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE+LOCATION_EXTRA,LOCATION_ONFIELD+LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE+LOCATION_EXTRA)
+		Duel.ConfirmCards(tp,g)
+		local tg=g:Filter(Card.IsCode,nil,sg:GetFirst():GetCode())
+		if #tg>0 then
+			Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)
+		end
+		Duel.ShuffleDeck(1-tp)
+	end
+end
+function s.tcon(e,tp,eg,ep,ev,re,r,rp)
+	return tp~=Duel.GetTurnPlayer()
+end
+function s.tcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.ttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local tg=Duel.GetAttacker()
+	if chkc then return chkc==tg end
+	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,1,0,0)
+end
+function s.top(e,tp,eg,ep,ev,re,r,rp)
+	local tg,d=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS,CHAININFO_TARGET_PARAM)
+	local tc=tg:GetFirst()
+	if tc:IsRelateToEffect(e) and tc:CanAttack() and not tc:IsStatus(STATUS_ATTACK_CANCELED) then
+		if Duel.SendtoHand(tc,nil,REASON_EFFECT) then
+	local sg=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+	if #sg>0 then
+		Duel.HintSelection(sg)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(500)
+		sg:GetFirst():RegisterEffect(e1)
+end
+end
+end
+end
