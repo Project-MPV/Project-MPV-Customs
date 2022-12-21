a91fb63f39a1aa4d541bdcc6d982d8cb6b5d2fab
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2022-12-21 15:44:16 +0700

    added the script
    for Bahamut.cdb

---
+++ script/c200009319.lua
@@ -0,0 +1,51 @@
+--Bahamut - Mega Flare
+local s,id=GetID()
+function s.initial_effect(c)
+	--Dragon monsters cannot be targeted
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(s.tgtg)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	--ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(s.atkcost)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
+end
+function s.tgtg(e,c)
+	return c:IsRace(RACE_DRAGON) and c~=e:GetHandler()
+end
+function s.atkfilter(c)
+	return c:GetLevel()>0
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.atkfilter,1,false,nil,c) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.atkfilter,1,1,false,nil,c)
+	local atk=g:GetFirst():GetLevel()
+	if atk<0 then atk=0 end
+	e:SetLabel(atk)
+	Duel.Release(g,REASON_COST)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		--Increase ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(e:GetLabel()*200)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
---
+++ script/c200009334.lua
@@ -0,0 +1,29 @@
+--Bahamut ZERO
+local s,id=GetID()
+function s.initial_effect(c)
+	--Synchro Summon
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_SYNCHRO),1,1,aux.FilterSummonCode(200009310),1,1)
+	c:EnableReviveLimit()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
+	local c=e:GetHandler()
+	Duel.ConfirmDecktop(tp,5)
+	local g=Duel.GetDecktopGroup(tp,5)
+	local ct=g:FilterCount(Card.IsRace,nil,RACE_DRAGON)
+	if ct==0 then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetValue(ct*500)
+	c:RegisterEffect(e1)
+end
