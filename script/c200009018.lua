355818fbcb4175994a7b50c9352de9e322f007f4
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2022-12-25 03:27:17 +0700

    Create c200009018.lua
    "White Rose Enchantress" , tbh, how you managed to treat it as a Plant ?

---
+++ script/c200009018.lua
@@ -0,0 +1,28 @@
+--White Rose Enchantress
+local s,id=GetID()
+function s.initial_effect(c)
+	--draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAIN_SOLVED)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(s.val)
+	c:RegisterEffect(e2)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	if not re:IsHasType(EFFECT_TYPE_ACTIVATE) or not re:IsActiveType(TYPE_COUNTER) then return end
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.BreakEffect()
+	Duel.Draw(tp,1,REASON_EFFECT)
+end
+function s.val(e,c)
+	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,LOCATION_MZONE)*500
+end
