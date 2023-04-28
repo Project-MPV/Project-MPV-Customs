90c91884e0715817afa38bc54631dc663f7bfb23
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-04-28 22:26:48 +0700

    Update cards_specific_functions.lua
    just testing some function

--- cards_specific_functions.lua
+++ cards_specific_functions.lua
@@ -1,4 +1,4 @@
 --Special Summon limit for Darklight Fusion monsters
 function Auxiliary.DarklightLimit(e,se,sp,st)
-	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or se:GetHandler():IsCode(117106529)
+	return se:GetHandler():IsCode(117106529)
 end
