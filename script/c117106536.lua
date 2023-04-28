6249f2ce1944cde8527836fb64ad352917a2b4e5
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-04-28 22:31:19 +0700

    Testing function

--- script/c117106536.lua
+++ script/c117106536.lua
@@ -8,7 +8,7 @@ c:SetUniqueOnField(1,0,id)
 	--Special Summon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetRange(LOCATION_EXTRA)
 	e1:SetValue(aux.DarklightLimit)
