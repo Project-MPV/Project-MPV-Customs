30cbe2ed57e0cfcf6e6f41139b1aa1f0fd790c74
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-04-30 23:07:53 +0700

    Adding scripts for Venom MPV
    Venom Naja Nivea, Venom Drip, Venom Fountain, Venom Mist, Venom Rebirth
    NOTE: For "Venom Cobra" i change it to Venom Naja Nivea, cause there is already existing name for that card.

---
+++ script/c822000005.lua
@@ -0,0 +1,36 @@
+--Venom Rebirth
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x50}
+s.counter_place_list={0x1009}
+function s.filter(c,e,tp)
+	return (c:IsSetCard(0x50) or c:IsCode(72677437)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) 
+end
+  local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+  local tc=g:GetFirst() 
+  tc:AddCounter(0x1009,1)
+end
