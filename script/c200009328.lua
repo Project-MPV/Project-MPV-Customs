810556c2f542b4c2091c787e145564f6628f2c77
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-05-08 04:05:08 +0700

    Add Nekroz of the Dark Storm - Bahamut
    Who ask the idea for Self-Ritual Summon ???, lol

---
+++ script/c200009328.lua
@@ -0,0 +1,97 @@
+--Nekroz of the Dark Storm - Bahamut
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Special summon procedure (Self-Ritual Summon)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Destroy or Change ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	--pierce
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x7530,0xb4}
+function s.mfilter(c)
+	return c:HasLevel() and c:IsAbleToGrave()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsPublic() and c:GetFlagEffect(id)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	and Duel.IsExistingMatchingCard(s.mfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	c:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_EXTRA,0,nil)
+	if chk==0 then return g:CheckWithSumEqual(Card.GetLevel,8,1,e:GetHandler():GetLevel()) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	local mat=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_EXTRA,0,nil)
+	if mat:CheckWithSumEqual(Card.GetLevel,8,1,e:GetHandler():GetLevel()) then
+	local tg=mat:SelectWithSumEqual(tp,Card.GetLevel,8,1,e:GetHandler():GetLevel())
+	if #tg>0 then
+	Duel.ReleaseRitualMaterial(tg)
+	Duel.SendtoGrave(tg,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+	Duel.BreakEffect()
+		Duel.SpecialSummon(c,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
+		c:CompleteProcedure()
+		--Cannot be targeted by monster effects
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3003)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(s.efilter)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_SELF_TURN,1)
+		c:RegisterEffect(e1)
+end
+end
+end
+function s.efilter(e,re,rp)
+	return re:IsActiveType(TYPE_MONSTER)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+end
+end
