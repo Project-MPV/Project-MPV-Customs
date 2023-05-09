33d6e18516bd6b1092fe81c32977de37ce1cd585
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-05-10 03:21:57 +0700

    Added or errata Ancient Gear Golem Xross
    I'll be honest with ya, the requester really  deserve to get bonked

---
+++ script/c5002.lua
@@ -0,0 +1,98 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(s.matfilter),3,nil,s.matcheck)	
+	--Cannot be destroyed by effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetValue(s.indesval)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--actlimit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,1)
+	e3:SetValue(s.aclimit)
+	e3:SetCondition(s.actcon)
+	c:RegisterEffect(e3)
+	--ATK increase
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_MATERIAL_CHECK)
+	e4:SetValue(s.matcheck2)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x7}
+function s.matfilter(c,g,lc,sumtype,tp)
+	return c:IsLevelAbove(8) and c:IsSetCard(0x7) 
+end
+function s.matcheck(g,lc,sumtype,tp)
+	return (g:IsExists(Card.IsSetCard,1,nil,0x7,lc,sumtype,tp) and g:IsExists(Card.IsType,1,nil,TYPE_FUSION,lc,sumtype,tp))
+end
+function s.indesval(e,re)
+	return re:IsActiveType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x7) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function s.filter(c)
+	return c:IsSetCard(0x7) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler(),e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler(),e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
+function s.aclimit(e,re,tp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.actcon(e)
+	return Duel.GetAttacker()==e:GetHandler()
+end
+function s.matfilter2(c)
+	return c:IsMonster() and c:GetLevel()>=0
+end
+function s.matcheck2(e,c)
+	local g=c:GetMaterial():Filter(s.matfilter2,nil)
+	local atk=g:GetSum(Card.GetOriginalLevel)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(atk*1000)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	c:RegisterEffect(e1)
+end
