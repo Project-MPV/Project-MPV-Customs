358f4847ce75d81f5d8af4439ab8fa4bae6b0ec7
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-05-11 22:39:51 +0700

    Update c399689443.lua

--- script/c399689443.lua
+++ script/c399689443.lua
@@ -48,10 +48,15 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 		if #g==0 then return end
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetTarget(s.atktg)
+		e1:SetTargetRange(LOCATION_MZONE,0)
 		e1:SetValue(500)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
 	end
+end
+function s.atktg(e,c)
+	return c:IsMonster()
 end
