11841988b22b67f831d25b89dbe65dbe1d36f54d
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-05-17 18:03:51 +0700

    Updates Bahamut
    Add Bahamut - Divine Albion and Bahamut - Divine Ddraig

---
+++ script/c200009330.lua
@@ -0,0 +1,73 @@
+--Bahamut - Divine Albion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion summon
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,200009310,s.ffilter)
+	--Luminous Divine Discharge
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+	--unaffectable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetValue(s.efilter)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetValue(s.efilter)
+	c:RegisterEffect(e3)
+	--cannot release
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e4:SetValue(aux.TargetBoolFunction(aux.NOT(Card.IsSetCard),0x7530))
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e5)
+end
+s.listed_series={0x7530}
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsRace(RACE_WARRIOR,fc,sumtype,tp) 
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,e:GetHandler())
+	local c=e:GetHandler()
+	for tc in aux.Next(g) do
+		local atk=tc:GetAttack()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(math.ceil(atk/2))
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			--Increase ATK
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_UPDATE_ATTACK)
+			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e2:SetValue(math.ceil(atk/2))
+			c:RegisterEffect(e2)
+end
+end
+end
+
+function s.efilter(e,te)
+	return not te:GetOwner():IsSetCard(0x7530)
+end
