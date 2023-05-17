865f4673c39c1da0e486f8fa4df337a658017714
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-05-17 20:05:32 +0700

    Add Annihilation Bahamut - Dragon Above All
    Look, since its material is unaffected, that means it can't be Fusion Summoned Normally (except if its Face-down thought)

---
+++ script/c200009332.lua
@@ -0,0 +1,86 @@
+--Annihilation Bahamut - Dragon Above All
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	Fusion.AddProcMix(c,true,true,200009330,200009331)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop)
+	c:EnableReviveLimit()
+	--What's up Bitch!
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+	--unaffected
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.econ)
+	e2:SetValue(s.efilter)
+	c:RegisterEffect(e2)
+	--cannot release
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x7530}
+s.material_setcode=0x7530
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+end
+function s.contactop(g)
+	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,e:GetHandler())
+	local c=e:GetHandler()
+	for tc in aux.Next(g) do
+		local atk=tc:GetAttack()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(math.ceil(atk/2))
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			--Increase ATK
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_UPDATE_ATTACK)
+			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e2:SetValue(math.ceil(atk/2))
+			c:RegisterEffect(e2)
+		if c:IsRelateToEffect(e) and c:IsFaceup() and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e3:SetValue(c:GetAttack()*2)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			c:RegisterEffect(e3)
+end
+end
+end
+end
+function s.econ(e)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+end
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
+end
