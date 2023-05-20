1fe7386fe0f6c7830c5116a376f47e9066dd7574
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-05-20 09:10:59 +0700

    Add Ancient White Rose Fairy Dragon
    It last effect still under maintenance, it can only change a level, but his previous effect still work fine.

--- script/c200000190.lua
+++ script/c200000190.lua
@@ -1,5 +1,148 @@
+--Ancient White Rose Fairy Dragon
 local s,id=GetID()
 function s.initial_effect(c)
-    Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),2,99)
+    Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),2,2)
     c:EnableReviveLimit()
+    --destroy S/T
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.descon)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	--negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_NEGATE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.negcon)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2)
+	--lvchange (Incomplete)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_LVCHANGE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(s.lvtg)
+	e3:SetOperation(s.lvop)
+	c:RegisterEffect(e3)
 end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDestructable() and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_SZONE,LOCATION_SZONE,nil)
+	if #g>0 then
+		g:AddCard(e:GetHandler())
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and re:GetHandler()~=e:GetHandler()
+		and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+	end
+end
+function s.lvfilter1(c)
+	return c:IsFaceup() and (c:HasLevel() or c:GetRank())
+end
+function s.lvfilter2(c)
+	return c:IsFaceup() and (c:HasLevel() or c:GetRank())
+end
+function s.rkcon(c)
+	return c:IsFaceup() and c:GetRank()
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local b1=Duel.IsExistingMatchingCard(s.lvfilter1,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.GetFlagEffect(tp,id)==0
+	local b2=Duel.IsExistingMatchingCard(s.lvfilter2,tp,0,LOCATION_MZONE,1,nil)
+		and Duel.GetFlagEffect(tp,id+1)==0
+	if chk==0 then return b1 or b2 end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_LVCHANGE,nil,1,tp,LOCATION_MZONE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_LVCHANGE,nil,1,tp,LOCATION_MZONE)
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp,g)
+local b1=Duel.IsExistingMatchingCard(s.lvfilter1,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.GetFlagEffect(tp,id)==0
+local b2=Duel.IsExistingMatchingCard(s.lvfilter2,tp,0,LOCATION_MZONE,1,nil)
+		and Duel.GetFlagEffect(tp,id+1)==0
+	local op=0
+	if b1 and b2 then op=Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4))
+	elseif b1 then op=Duel.SelectOption(tp,aux.Stringid(id,3))
+	elseif b2 then op=Duel.SelectOption(tp,aux.Stringid(id,4))+1
+	else return end
+if op==0 then
+	local lv=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local g=Duel.GetMatchingGroup(s.lvfilter1,tp,LOCATION_MZONE,0,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LVRANK)
+	local lv=Duel.AnnounceLevel(tp,1,12) 
+	Duel.SetTargetParam(lv)
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_RANK)
+		e2:SetCondition(s.rkcon)
+		e2:SetValue(lv)
+		tc:RegisterEffect(e1)
+		local tclv=tc:GetLevel()
+	--i still don't know hot to set it
+			Duel.Recover(tp,math.abs(lv-tclv)*500,REASON_EFFECT)
+	
+	end
+		Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,1)
+	else
+	local lv=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local g=Duel.GetMatchingGroup(s.lvfilter2,tp,0,LOCATION_MZONE,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LVRANK)
+	local lv=Duel.AnnounceLevel(tp,1,12)
+	Duel.SetTargetParam(lv)
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_RANK)
+		e2:SetCondition(s.rkcon)
+		e2:SetValue(lv)
+		tc:RegisterEffect(e1)
+		local tclv=tc:GetLevel()
+	--i still don't know hot to set it
+			Duel.Recover(tp,math.abs(lv-tclv)*500,REASON_EFFECT)
+	end
+		Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,1)
+	end
+end
+
+
+
+
