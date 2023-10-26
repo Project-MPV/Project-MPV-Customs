e64c7e81344ed51558b104357fb2a1ec31f27151
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2023-10-26 11:29:47 +0700

    Add scripts

---
+++ script/c201998980.lua
@@ -0,0 +1,53 @@
+--Imaginary Force - Reversal Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	--Gains ATK and return
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x303}
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_MZONE,0,nil,TYPE_MONSTER)
+	return Duel.IsExistingMatchingCard(Card.IsAttribute,tp,LOCATION_MZONE,0,2,nil,ATTRIBUTE_LIGHT) and g:GetClassCount(Card.GetRace)>=2	
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(500)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	if c:RegisterEffect(e1)~=0 then
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	if #g>0 then
+		local tg=g:GetMinGroup(Card.GetAttack)
+		if #tg>1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+			local sg=tg:Select(tp,1,1,nil)
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		else Duel.SendtoHand(tg,nil,REASON_EFFECT) end
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e2:SetOperation(s.ssop)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+	e2:SetCountLimit(1)
+	c:RegisterEffect(e2)
+	end
+end
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SendtoDeck(e:GetHandler(),nil,1,REASON_EFFECT)
+end
