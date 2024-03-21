297b3d8366223e71460a2977d5b27f395adc78a6
Fawwazzed777 <fawwazzed@gmail.com>
2024-03-21 11:51:03 +0700

    Updates strings

--- cards_specific_functions.lua
+++ cards_specific_functions.lua
@@ -1,4 +1,4 @@
---Special Summon limit for Darklight Fusion monsters
-function Auxiliary.DarkLightLimit(e,se,sp,st)
+--Special Summon limit for "Darklight Fusion"-related Fusion monsters
+function Auxiliary.DarkLightFLimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or se:GetHandler():IsCode(117106529)
 end
