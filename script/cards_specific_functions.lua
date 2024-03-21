26bb45cb8e4c6f959c13b9acfbfdb3c451ec4007
Fawwazzed777 <fawwazzed@gmail.com>
2024-03-21 12:53:20 +0700

    Test

--- script/cards_specific_functions.lua
+++ script/cards_specific_functions.lua
@@ -2,4 +2,3 @@
 function Auxiliary.DarkLightFLimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or se:GetHandler():IsCode(117106529)
 end
---NOTE: Use this 'Duel.LoadScript("user_cards_specific_functions.lua")' in every script to connect all function on this script
