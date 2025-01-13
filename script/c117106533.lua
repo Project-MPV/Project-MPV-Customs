39a38911f4f08bc10af677878f6eddfae7497859
Fawwazzed777 <fawwazzed@gmail.com>
2025-01-13 07:40:22 +0700

    Fix Something

--- script/c117106533.lua
+++ script/c117106533.lua
@@ -43,8 +43,7 @@ function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.thfilter(c)
 	return c:IsAbleToHand() and (c:IsFaceup() or not c:IsLocation(LOCATION_EXTRA))
-		and ((c:IsSetCard(0x344) and c:IsType(TYPE_SPELL|TYPE_TRAP))
-		or (c:IsSetCard(0x344) and c:IsMonster()))
+		and c:IsSetCard(0x344)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
