7c5a05a3e14cd1acf5163c3c49c31cebebf695aa
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-23 17:02:57 +0700

    Updates

---
+++ script/c649968948.lua
@@ -0,0 +1,112 @@
+--Exfrost Dragon Gaze
+Duel.EnableUnofficialProc(PROC_STATS_CHANGED)
+Duel.LoadScript("c511000883.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e=Effect.CreateEffect(c)
+	e:SetType(EFFECT_TYPE_ACTIVATE)
+	e:SetCode(EVENT_FREE_CHAIN)
+	e:SetHintTiming(0,TIMING_END_PHASE)
+	c:RegisterEffect(e)
+	--Must attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetCondition(s.atkcon1)
+	e1:SetCode(EFFECT_MUST_ATTACK)
+	c:RegisterEffect(e1)
+	--Bind
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.con)
+	e2:SetTarget(s.tg)
+	e2:SetOperation(s.op)
+	c:RegisterEffect(e2)
+	--If WATER Monster loses ATK
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCode(511000883)
+	e3:SetCondition(s.atkcon)
+	e3:SetTarget(s.atktg)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0xfc13}
+function s.atkcon1(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_WATER),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	local bc=Duel.GetAttackTarget()
+	if not bc then return false end
+	if tc:IsControler(1-tp) then tc,bc=bc,tc end
+	if tc:IsFaceup() and (tc:IsSetCard(0xfc13) and tc:IsType(TYPE_SYNCHRO)) or 
+	(tc:GetOriginalLevel()>=8 and tc:IsAttribute(ATTRIBUTE_WATER) and tc:IsType(TYPE_SYNCHRO)) then
+		e:SetLabelObject(bc)
+		return true
+	else return false end
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local bc=e:GetLabelObject()
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,0,0)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local bc=e:GetLabelObject()
+	if bc and bc:IsRelateToBattle() then
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	bc:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DISABLE_EFFECT)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	bc:RegisterEffect(e2)
+	local at=Effect.CreateEffect(e:GetHandler())
+	at:SetDescription(3206)
+	at:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	at:SetType(EFFECT_TYPE_SINGLE)
+	at:SetCode(EFFECT_CANNOT_ATTACK)
+	at:SetValue(1)
+	at:SetReset(RESET_EVENT+RESETS_STANDARD)
+	bc:RegisterEffect(at)
+end
+end
+function s.cfilter(c,tp)
+	local val=0
+	if c:GetFlagEffect(284)>0 then val=c:GetFlagEffectLabel(284) end
+	return c:IsControler(tp) and c:GetAttack()~=val and c:IsAttribute(ATTRIBUTE_WATER)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if c then
+	local kk=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #kk>0 then
+		Duel.HintSelection(kk)
+		Duel.Destroy(kk,REASON_EFFECT)
+end
+end
+end
