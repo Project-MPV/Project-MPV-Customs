1a9702f8b7e7bc98a5c1c662f3ce997a2514b7cc
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-23 19:01:52 +0700

    Updates

---
+++ script/c567049999.lua
@@ -0,0 +1,107 @@
+--Enigmation - Phantasm Nexus
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x344),10,2,nil,nil,Xyz.InfiniteMats)
+	c:EnableReviveLimit()
+	--to deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.tdtg)
+	e1:SetOperation(s.tdop)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id+1)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.tg)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.tdfilter(c)
+	return c:IsType(TYPE_XYZ) and c:IsAbleToDeck()
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
+	ct=Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	if ct>0 then
+		Duel.Recover(tp,ct*700,REASON_EFFECT)
+		Duel.Damage(1-tp,ct*700,REASON_EFFECT)
+end
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.tffilter(c)
+	return not c:IsType(TYPE_TOKEN) 
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tffilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.tffilter,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local seq=tc:GetSequence()
+	if tc:IsControler(1-tp) then seq=seq+16 end
+	if tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEDOWN,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END,2)
+		e1:SetReset(RESET_PHASE+PHASE_END,2)
+		e1:SetLabelObject(tc)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.rtcon)
+		e1:SetOperation(s.retop)
+		Duel.RegisterEffect(e1,tp)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_DISABLE_FIELD)
+		e2:SetLabel(seq)
+		e2:SetLabelObject(tc)
+		e2:SetCondition(s.discon)
+		e2:SetOperation(s.disop)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function s.rtcon(e,tp,eg,ep,ev,re,r,rp)
+	return 1-tp==Duel.GetTurnPlayer()
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ReturnToField(e:GetLabelObject())
+		local g1=Duel.GetFieldGroup(tp,0,LOCATION_HAND):RandomSelect(tp,1)
+		Duel.Remove(g1,POS_FACEDOWN,REASON_EFFECT)
+		local g2=Duel.GetFieldGroup(tp,0,LOCATION_DECK):RandomSelect(tp,1)
+		Duel.Remove(g2,POS_FACEDOWN,REASON_EFFECT)
+		local g3=Duel.GetFieldGroup(tp,0,LOCATION_GRAVE):RandomSelect(tp,1)
+		Duel.Remove(g3,POS_FACEDOWN,REASON_EFFECT)
+end
+function s.discon(e,c)
+	if e:GetLabelObject():IsLocation(LOCATION_REMOVED) then
+		return true
+	else
+		e:Reset()
+		return false
+	end
+end
+function s.disop(e,tp)
+	local dis1=(0x1<<e:GetLabel())
+	return dis1
+end
+
