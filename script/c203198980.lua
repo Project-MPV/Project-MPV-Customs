c9c490735ba6655b9c895abb9a1138e0f6b23806
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-08 22:48:30 +0700

    Updates

---
+++ script/c203198980.lua
@@ -0,0 +1,92 @@
+--Enigmation Force - Luminvesper
+local s,id=GetID()
+function s.initial_effect(c)
+	--If this card battles a monster, neither can be destroyed by that battle
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(s.indestg)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--If returned to Deck or removed
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_DECK)
+	e2:SetCountLimit(1,{id,0})
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCondition(s.scon)
+	e2:SetTarget(s.stg)
+	e2:SetOperation(s.sop)
+	c:RegisterEffect(e2)
+	local ee=e2:Clone()
+	ee:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(ee)
+	--Grant effect when used as material for "Enigmation"or "Imaginary Force" fusion,synchro or xyz monster
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(s.efcon)
+	e3:SetOperation(s.efop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x303,0x344}
+function s.indestg(e,c)
+	local handler=e:GetHandler()
+	return c==handler or c==handler:GetBattleTarget()
+end
+function s.scon(e,tp,eg,ep,ev,re,r,rp)
+	if not re then return false end
+	local rc=re:GetHandler()
+	return (rc:IsSetCard(0x303) or rc:IsSetCard(0x344))
+end
+function s.stg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.sop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.SendtoHand(c,nil,REASON_EFFECT)
+end
+function s.efcon(e,tp,eg,ep,ev,re,r,rp)
+	local p=e:GetHandler()
+	return (r&REASON_FUSION+REASON_SYNCHRO+REASON_XYZ)~=0
+		and (p:GetReasonCard():IsSetCard(0x303) or p:GetReasonCard():IsSetCard(0x344))
+end
+function s.efop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	local e0=Effect.CreateEffect(rc)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_ADD_RACE)
+	e0:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e0:SetValue(RACE_ILLUSION)
+	rc:RegisterEffect(e0,true)
+	local e1=Effect.CreateEffect(rc)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(s.drop)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	rc:RegisterEffect(e1,true)
+	if not rc:IsType(TYPE_EFFECT) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_ADD_TYPE)
+		e2:SetValue(TYPE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		rc:RegisterEffect(e2,true)
+	end
+end
+function s.drop(e,c,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	local handler=e:GetHandler()
+	return c==handler or c==handler:GetBattleTarget()
+end
