a5371cddc4d3458d28447cabb9da41a5c753e61d
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-26 20:54:27 +0700

    Nerf
    This single continuous spell was just too good, search 1 while also mill 1, i admit this is probably the most "auto-pilot" card in this archetype.
    The purpose of this card is to help IF recover their hand after returning their own card, but only for it to become redundant (i guess).

--- script/c200898980.lua
+++ script/c200898980.lua
@@ -6,20 +6,20 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
-	--Add 1 "Imaginary Force" from deck
+	--Search when your card returned by IF effect
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_TO_DECK)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-	--cannot be target
+	--Opponent cannot target your Fusion monsters
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
@@ -30,37 +30,47 @@ function s.initial_effect(c)
 	e2:SetValue(aux.tgoval)
 	c:RegisterEffect(e2)
 end
-function s.rtfilter(c,e,tp)
-	return c:IsPreviousLocation(LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_REMOVED) and c:IsReason(REASON_EFFECT)
+s.listed_series={0x303}
+--card returned to Deck/Extra by IF effect, and you control IF monster
+function s.cfilter(c,tp)
+	return c:IsControler(tp)
+		and c:IsReason(REASON_EFFECT)
+		and c:IsReason(REASON_COST)
+		and c:IsLocation(LOCATION_DECK+LOCATION_EXTRA)
+		and not c:IsPreviousLocation(LOCATION_HAND)
 end
-function s.rvfilter(c)
-	return c:IsSetCard(0x303)
+
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	if not re or not re:GetHandler():IsSetCard(0x303) then return false end
+	return eg:IsExists(s.cfilter,1,nil,tp)
+		and Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_MZONE,0,1,nil,0x303)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.rtfilter,1,nil,e,tp) and 
-	Duel.IsExistingMatchingCard(s.rvfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil) 
-end
-function s.filter(c)
+
+function s.thfilter(c)
 	return c:IsSetCard(0x303) and not c:IsCode(id) and c:IsAbleToHand()
 end
+
 function s.tgfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x303) and c:IsAbleToGrave()
+	return c:IsSetCard(0x303) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
-	if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-	local sg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #sg>0 then
-		Duel.SendtoGrave(sg,REASON_EFFECT)
-end
-end
-end
+		if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+			local sg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+			if #sg>0 then
+				Duel.SendtoGrave(sg,REASON_EFFECT)
+			end
+		end
+	end
 end
