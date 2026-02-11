73fc85fc93d0eb085eac04e1b13fcdb559e0347e
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-11 17:37:03 +0700

    Create c196488227.lua

---
+++ script/c196488227.lua
@@ -0,0 +1,91 @@
+--Enigmation Force - Crest Keeper, Varniel
+local s,id=GetID()
+function s.initial_effect(c)
+Pendulum.AddProcedure(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Place in Pendulum Zone
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_EXTRA)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.pencon)
+	e3:SetTarget(s.pentg)
+	e3:SetOperation(s.penop)
+	c:RegisterEffect(e3)
+	--To Bottom
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_PZONE)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetTarget(s.tetg)
+	e4:SetOperation(s.teop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x303,0x344}
+function s.filter(c,code)
+	return c:IsFaceup() and c:IsCode(code)
+end
+function s.thfilter(c,tp)
+	return (c:IsSetCard(0x303) or c:IsSetCard(0x344)) and c:IsAbleToHand()
+		and not Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_ONFIELD,0,1,nil,c:GetCode())
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+end
+end
+function s.penconfilter(c)
+	return c:IsFaceup() and c:IsMonster() and (c:IsSetCard(0x303) or c:IsSetCard(0x344))
+end
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.penconfilter,tp,LOCATION_MZONE,0,1,nil) 
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1))
+		and Duel.IsExistingMatchingCard(s.penconfilter,tp,LOCATION_MZONE,0,1,nil)  end
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.CheckLocation(tp,LOCATION_PZONE,0) and not Duel.CheckLocation(tp,LOCATION_PZONE,1) then return end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+--
+function s.rpfilter(c,e,tp)
+	return (c:IsSetCard(0x344) or c:IsSetCard(0x303)) and (c:IsLevelBelow(5) or c:IsRankBelow(5)) 
+	and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.tetg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return not e:GetHandler():IsForbidden() and Duel.IsExistingMatchingCard(s.rpfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+end
+function s.teop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoDeck(c,nil,1,REASON_EFFECT)
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+		local g=Duel.SelectMatchingCard(tp,s.rpfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+		local tc=g:GetFirst()
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
