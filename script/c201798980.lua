301a41d08bfe23c47db786283f8efb19be5d2632
Fawwazzed777 <fawwazzed@gmail.com>
2026-07-07 13:58:33 +0700

    Erratta
    Imaginary Force - Hierarchia Amireita

--- script/c201798980.lua
+++ script/c201798980.lua
@@ -15,18 +15,20 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
+	e1:SetCountLimit(1,{id,0})
 	e1:SetTarget(s.tdtg)
 	e1:SetOperation(s.tdop)
 	c:RegisterEffect(e1)
-	--Weirdly destroy replace
+	--Your opponent "Shuffle 1 card you control into the Deck"
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetDescription(aux.Stringid(id,3))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(s.reptg)
-	e2:SetValue(s.repval)
-	e2:SetOperation(s.repop)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.chcon)
+	e2:SetTarget(s.chtg)
+	e2:SetOperation(s.chop)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x303}
@@ -60,37 +62,22 @@ function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.RegisterEffect(e1,tp)
 		aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,2),nil)
 end
-function s.repfilter(c,tp)
-	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and (c:IsSetCard(0x303) and c:IsType(TYPE_MONSTER))
-		and (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT)) and not c:IsReason(REASON_REPLACE)
+function s.chcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp
 end
-function s.dsfilter(c,e,tp)
-	return c:IsLocation(LOCATION_GRAVE) and c:IsSetCard(0x303)
-		and c:IsAbleToDeck(e) and not c:IsStatus(STATUS_DESTROY_CONFIRMED+STATUS_BATTLE_DESTROYED)
+function s.chtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
 end
-function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp)
-		and Duel.IsExistingMatchingCard(s.dsfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
-		local g=Duel.SelectMatchingCard(tp,s.dsfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-		e:SetLabelObject(g:GetFirst())
-		g:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,true)
-		return true
+function s.chop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeChainOperation(ev,s.changeop)
+end
+function s.changeop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_ONFIELD,0,1,nil) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_ONFIELD,0,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g)
+			Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+		end
 	end
-	return false
-end
-function s.repval(e,c)
-	return s.repfilter(c,e:GetHandlerPlayer())
-end
-function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	tc:SetStatus(STATUS_DESTROY_CONFIRMED,false)
-	if Duel.SendtoDeck(tc,nil,2,REASON_EFFECT+REASON_REPLACE)~=0 then
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,1,nil)
-	if #g>0 then
-	Duel.HintSelection(g)
-	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
-end
-end
 end
